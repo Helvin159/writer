ec9a75232cd0a5f176d1b8608815b9675cc4a06d
Helvin159 <helvinrymer@Helvins-MacBook-Pro.local>
2020-11-10 18:47:14 -0500

    updates

--- wp-config.php
+++ wp-config.php
@@ -20,16 +20,17 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'local' );
-
-/** MySQL database username */
-define( 'DB_USER', 'root' );
-
-/** MySQL database password */
-define( 'DB_PASSWORD', 'root' );
-
-/** MySQL hostname */
-define( 'DB_HOST', 'localhost' );
+if(strstr($_SERVER['SERVER_NAME'], 'danielle.local')){
+	define( 'DB_NAME', 'local' );
+	define( 'DB_USER', 'root' );
+	define( 'DB_PASSWORD', 'root' );
+	define( 'DB_HOST', 'localhost' );
+}else{
+	define( 'DB_NAME', 'kmmkcamy_danielle' );
+	define( 'DB_USER', 'kmmkcamy_helvin' );
+	define( 'DB_PASSWORD', 'Dora0411!?!' );
+	define( 'DB_HOST', 'localhost' );
+}
 
 /** Database Charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
