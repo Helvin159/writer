b0905bcc77ef7cf97f74903ed2b1ce97cd70499f
Helvin159 <helvinrymer@Helvins-MacBook-Pro.local>
2020-11-13 14:33:04 -0500

    Updates

--- wp-content/themes/writer/archive-prevwork.php
+++ wp-content/themes/writer/archive-prevwork.php
@@ -22,7 +22,7 @@
                             <h3><a href="<?php the_permalink() ?>"><?php the_title()?></a></h3>
                             <p class="px-2">
                                 <?php 
-                                    echo    substr(get_the_excerpt(),0,150);                         
+                                    echo    substr(get_the_excerpt(),0,140);                         
                                 ?>  
                                 <a href="<?php the_permalink()?>">[...]</a>
                             </p>
