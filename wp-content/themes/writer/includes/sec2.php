28b4be40fb3d1bba5932bd5fb8a4878a2a2d0aaa
Helvin159 <helvinrymer@Helvins-MBP.home>
2020-11-13 20:38:30 -0500

    Updates

--- wp-content/themes/writer/includes/sec2.php
+++ wp-content/themes/writer/includes/sec2.php
@@ -6,7 +6,7 @@
     </div>
     <div class="row py-5">
         <div class="col-md-6 p-5">
-            <div class="container">
+            <div class="container d-none d-sm-none d-md-block d-lg-block">
                 <h4>Increse Conversion Rates</h4>
                 <p class="text-justify">
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia nemo reprehenderit, sed distinctio commodi soluta! Provident odio exercitationem ipsam dolores adipisci iusto, qui quo dolor ea eaque totam praesentium placeat!
@@ -16,18 +16,18 @@
         </div>
         <div class="col-md-6 p-5">
             <div class="container p-5">
-                <img class="img-fluid d-none d-sm-none d-md-block d-lg-block" src="<?php echo get_theme_file_uri('assets/img/a3.png')?>" alt="">
+                <img class="img-fluid" src="<?php echo get_theme_file_uri('assets/img/a3.png')?>" alt="">
             </div>
         </div>
     </div>
     <div class="row  py-5 secTwoRowTwo">
         <div class="col-md-6 p-5 unskew">
             <div class="container px-5">
-                <img class="img-fluid d-none d-sm-none d-md-block d-lg-block" src="<?php echo get_theme_file_uri('assets/img/a3.png')?>" alt="">
+                <img class="img-fluid" src="<?php echo get_theme_file_uri('assets/img/a3.png')?>" alt="">
             </div>
         </div>
         <div class="col-md-6 p-5 unskew">
-            <div class="container">
+            <div class="container d-none d-sm-none d-md-block d-lg-block">
                 <h4>Reduce Bounce Rates</h4>
                 <p class="text-justify">
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia nemo reprehenderit, sed distinctio commodi soluta! Provident odio exercitationem ipsam dolores adipisci iusto, qui quo dolor ea eaque totam praesentium placeat!
@@ -38,7 +38,7 @@
     </div>
     <div class="row py-5">
         <div class="col-md-6 p-5">
-            <div class="container">
+            <div class="container d-none d-sm-none d-md-block d-lg-block">
                 <h4>Drive More Traffic</h4>
                 <p class="text-justify">
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia nemo reprehenderit, sed distinctio commodi soluta! Provident odio exercitationem ipsam dolores adipisci iusto, qui quo dolor ea eaque totam praesentium placeat!
@@ -48,7 +48,7 @@
         </div>
         <div class="col-md-6">
             <div class="container p-5">
-                <img class="img-fluid d-none d-sm-none d-md-block d-lg-block" src="<?php echo get_theme_file_uri('assets/img/a3.png')?>" alt="">
+                <img class="img-fluid " src="<?php echo get_theme_file_uri('assets/img/a3.png')?>" alt="">
             </div>
     </div>
         </div>
