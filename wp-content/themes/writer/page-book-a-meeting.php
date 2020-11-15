4932a7fe4a0925756108603c3ec9d2698e6deed4
Helvin159 <helvinrymer@Helvins-MacBook-Pro.local>
2020-11-14 21:40:46 -0500

    Calendly

--- wp-content/themes/writer/page-book-a-meeting.php
+++ wp-content/themes/writer/page-book-a-meeting.php
@@ -8,18 +8,21 @@
 
 <hr class="w-75">
 
-<div class="row  p-5 my-5">
+<div class="container-fluid">
+    <div class="row  p-5 my-5">
     <div class="col-md-6 p-5 d-none d-sm-none d-md-block d-lg-block">
         <div class="bookingImg">
             <img class="img-fluid" src="<?php echo get_theme_file_uri('assets/img/a3.png')?>" alt="">
         </div>
     </div>
     <div class="col-md-6 ">
-        <div class="calendlyWidget calendlyWidget p-1">
+        <div class="calendlyWidget p-1">
             <?php require_once('includes/calendly.php'); ?>
         </div>
     </div>    
+    </div>
 </div>
+
 
 
 <?php get_footer();
