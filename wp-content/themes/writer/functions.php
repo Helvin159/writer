de1e1770c8584c5bdbc70f1555234f422faa651f
Helvin159 <helvinrymer@Helvins-MacBook-Pro.local>
2020-11-21 13:32:11 -0500

    updates

--- wp-content/themes/writer/functions.php
+++ wp-content/themes/writer/functions.php
@@ -14,9 +14,9 @@ function writer (){
   } else{
       // CSS
     //   wp_enqueue_style('my-style', get_theme_file_uri('/css/styles.css'), null, '1.0', 'all');
-      wp_enqueue_style('my-style', get_theme_file_uri('/bundled-assets/styles.9f87376f54f25dd91811.css'), null, '1.0', 'all');
+      wp_enqueue_style('my-style', get_theme_file_uri('/bundled-assets/styles.7da0148659e2777ee6c9.css'), null, '1.0', 'all');
       // wp_enqueue_script('our-vendors-js', get_theme_file_uri('/bundled-assets/undefined'), NULL, '1.0', true);
-      wp_enqueue_script('main-writer-scripts', get_theme_file_uri('/bundled-assets/scripts.9f87376f54f25dd91811.js'), NULL, '1.0', true);
+      wp_enqueue_script('main-writer-scripts', get_theme_file_uri('/bundled-assets/scripts.7da0148659e2777ee6c9.js'), NULL, '1.0', true);
     }
 
 
