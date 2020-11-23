ddb4bcb1e251b910ce5abbf499fa5447bdf99180
Helvin159 <helvinrymer@Helvins-MacBook-Pro.local>
2020-11-23 13:24:13 -0500

    updates

--- wp-content/themes/writer/includes/hero.php
+++ wp-content/themes/writer/includes/hero.php
@@ -3,5 +3,6 @@
                 <h1 >HELLO, I'M DANIELLE</h1>
                 <h3 >A COPYWRITER</h3>
                 <a href="<?php echo esc_url(site_url('/packages'))?>" class="btn btn-md btnDarkPurple lightGrey my-4">Let's Write Something</a>
+                
             </div>
         </div>
