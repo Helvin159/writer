7d26ed5911940dc49ae234fc0269535e0ad7a728
Helvin159 <helvinrymer@Helvins-MacBook-Pro.local>
2020-12-07 16:24:23 -0500

    updates

--- wp-content/themes/writer/includes/hero.php
+++ wp-content/themes/writer/includes/hero.php
@@ -1,7 +1,7 @@
 <div class="container-fluid frontHero">
             <div class="container beforeLoad fadeIn frontHeroText text-center">
-                <h1 >HELLO, I'M DANIELLE</h1>
-                <h3 >A COPYWRITER</h3>
+                <h1 ><?php echo get_field('hero_text_line_1')?></h1>
+                <h3 ><?php echo get_field('hero_text_line_2')?></h3>
                 <a href="<?php echo esc_url(site_url('/packages'))?>" class="btn btn-md btnDarkPurple lightGrey my-4">Let's Write Something</a>  
             </div>
         </div>
