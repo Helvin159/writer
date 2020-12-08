a0a08f2629d81abe00b63e2f7a3bf781ec86c35e
Helvin159 <helvinrymer@Helvins-MacBook-Pro.local>
2020-12-08 14:43:40 -0500

    about section 4

--- wp-content/themes/writer/archive-prevwork.php
+++ wp-content/themes/writer/archive-prevwork.php
@@ -3,17 +3,6 @@
     require_once('includes/banner.php');
 ?>
 <div class="container py-5">
-    <!-- <h1>My Previous Work</h1> -->
-    <hr class="w-25 bgPurple ">
-        <p class="text-center">
-            1234 Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit eveniet saepe architecto, odit repellendus quam dolor. Ratione magni in voluptatem rerum quibusdam, ea dolores accusantium. Lorem ipsum adipisicing elit. Amet eveniet accusamus aspernatur. Non consequatur nihil ab deleniti vitae necessitatibus soluta facilis, nam obcaecati! Error fugit aliquam quas consequuntur iste ducimus! Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos fuga iure laborum. Facilis quam possimus cum officiis iste similique suscipit asperiores dolores, ex assumenda itaque fugit earum saepe provident temporibus.
-        </p>
-</div>
-
-<?php 
-require_once('includes/about/aboutSec2.php');
-?>
-<div class="container py-5">
     <div class="row p-2">
         <?php  
             $paged = ( get_query_var( 'paged' ) ) ? get_query_var( 'paged' ) : 1;
--- wp-content/themes/writer/includes/banner.php
+++ wp-content/themes/writer/includes/banner.php
@@ -3,12 +3,14 @@
         <?php 
                 if($_SERVER['REQUEST_URI'] == '/blog/'){ ?>
                         <h1 class="display-3">Blog</h1>
-                    <?php } else if($_SERVER['REQUEST_URI'] == '/prevwork/'){ ?>
-                        <h1 class="display-3">My Previous Work</h1>
-                    <?php }else if($_SERVER['REQUEST_URI'] == '/services/'){ ?>
-                        <h1 class="display-3">Services</h1>
-                        <?php }else { ?>
-                        <h1 class="display-3"><?php echo substr(get_the_archive_title(),10)?></h1>
+                    <?php } else { ?>
+                        <?php if(get_post_type() == 'prevwork' or get_post_type() == 'services'){ ?>
+                            <h1 class="display-3"><?php echo substr(get_the_archive_title(),10)?></h1>
+                        <?php }else{ ?>
+                            <h1 class="display-3"><?php the_title()?></h1>
+                        <?php }
+                        ?>
+
                     <?php } ?>    
             </div>
         </div>
