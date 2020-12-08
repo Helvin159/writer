4565aff670fa4d6534f83df61817bbeb30c37b6f
Helvin159 <helvinrymer@Helvins-MacBook-Pro.local>
2020-12-08 13:43:40 -0500

    about section 4

--- wp-content/themes/writer/includes/about/aboutSec4.php
+++ wp-content/themes/writer/includes/about/aboutSec4.php
@@ -1,5 +1,5 @@
 <div class="container-fluid secFourAbout px-5 py-5 text-center">
-        <h1 class="lightGrey">I'm a sucker for fine words. The World needs more of them. <br> You can help.</h1>
+        <h1 class="lightGrey"><?php echo get_field('about_page_catchy_phrase')?> <br> <?php echo get_field('about_page_catchy_phrase_line_2')?></h1>
         <a href="<?php echo esc_url(site_url('/packages'))?>" class="btn btn-lg btnPurple lightGrey josefinLight">Get Started</a>
         
 </div>
