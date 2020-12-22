0a7c179894d0903302334711f2ac398dc5a0f50e
Helvin159 <helvinrymer@Helvins-MacBook-Pro.local>
2020-12-21 23:36:11 -0500

    google analytics

--- wp-content/themes/writer/header.php
+++ wp-content/themes/writer/header.php
@@ -2,14 +2,16 @@
 <html <?php language_attributes();?> >
   <head>
     <!-- Global site tag (gtag.js) - Google Analytics -->
-  <script async src="https://www.googletagmanager.com/gtag/js?id=G-X01CSF289V"></script>
-  <script>
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-XN5N7D5FQW"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
 
-    gtag('config', 'G-X01CSF289V');
-  </script>
+  gtag('config', 'G-XN5N7D5FQW');
+</script>
+
+
     <meta charset="<?php bloginfo('charset');?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 		<?php wp_head(); ?>
